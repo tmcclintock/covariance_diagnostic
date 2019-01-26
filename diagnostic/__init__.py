3bc9ea785f25d393c71a94ff38ad96b084c80da8
Tom McClintock <tmcclintock89@gmail.com>
2019-01-26 11:59:39 -0500

    Making the diagnostic tool.

---
+++ diagnostic/__init__.py
@@ -0,0 +1,5 @@
+from .diagnostic import *
+
+__version__ = "0.1.0"
+__author__  = "Thomas McClintock"
+__email__   = "mcclintock@bnl.gov"
---
+++ diagnostic/diagnostic.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class diagnostic(object):
+    """
+    Take in a covariance matrix and perform some diagnostics on it.
+    :param C:
+        2D array of a covariance matrix
+    """
+
+    def __init__(self, C):
+        C = np.array(C)
+
+        if C.ndim < 2:
+            raise Exception("Covariance matrix has too few dimensions.")
+        if C.ndim > 2:
+            raise Exception("Covariance matrix has too many dimensions.")
+        if not np.allclose(C, C.T, atol=1e-8):
+            raise Exception("Covariance matrix is not symmetric.")
+
+        #Compute eigenvalues
+        w, v = np.linalg.eig(C)
+        self.eigenvalues = w
+        self.eigenvectors = v
+
+        #See if it is positive definite
+        self.is_pos_def = np.all(self.eigenvalues > 0)
+
+        #Compute degrees of freedom
+
+    def recompute_DOF(self):
+        pass
